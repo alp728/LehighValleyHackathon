76bd771e63a043db003a1a6eb6c2c79adbb54207
Alex <42507764+ilumn@users.noreply.github.com>
2024-10-19 17:09:14 -0400

    e

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,8 +6,6 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     # FE_ROOT: str
     DATABASE_URL: str
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
     REGION_NAME: str
     BUCKET_NAME: str
 
